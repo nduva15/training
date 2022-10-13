c726212998461d9c6fd0396dad102e0f9e3f2528
nduva15 <timothynduva349@gmail.com>
2022-10-13 15:27:28 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:training/Privacy%20Policy.dart';
+import 'package:training/contacts.dart';
+import 'package:training/dashboard.dart';
+import 'package:training/events.dart';
+import 'package:training/feedback.dart';
+import 'package:training/notes.dart';
+import 'package:training/notifications.dart';
+import 'package:training/settings.dart';
 
 import 'my_drawer.dart';
 
@@ -30,66 +38,95 @@ class MyApp extends StatelessWidget {
     }
     
     class _HomePageState extends State<HomePage> {
-  var currentPage = DrawerSections.dashboard;
+      var currentPage = DrawerSections;
+
       @override
       Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green[700],
-            title: Text("Training"),
-          ),
-          body: Container(
-            child: const Center(
-              child: Text("Home Page", style: TextStyle(color: Colors.grey, fontSize: 22),),
+        var container;
+        if (currentPage == DrawerSections.dashboard) {
+          container = DashboardPage();
+        } else if (currentPage == DrawerSections.contacts) {
+          container = ContactsPage();
+        } else if (currentPage == DrawerSections.events) {
+          container = EventsPage();
+        } else if (currentPage == DrawerSections.notes) {
+          container = NotesPage();
+        } else if (currentPage == DrawerSections.settings) {
+          container = SettingsPage();
+        } else if (currentPage == DrawerSections.notifications) {
+          container = NotificationsPage();
+        }else if (currentPage == DrawerSections.privacy_policy) {
+          container = PrivacyPolicyPage();
+        }else if (currentPage == DrawerSections.send_feedback) {
+          container = SendFeedback();
+        }
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.green[700],
+              title: Text("Training"),
             ),
-          ),
-          drawer: Drawer(
-            child: SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  children: [
-                    MyHeaderDrawer(),
-                    MyDrawerList(),
-                  ],
-                ),
+            body: Container(
+              child: const Center(
+                child: Text("Home Page",
+                  style: TextStyle(color: Colors.grey, fontSize: 22),),
               ),
             ),
+            drawer: Drawer(
+              child: SingleChildScrollView(
+                child: Container(
+                  child: Column(
+                    children: [
+                      MyHeaderDrawer(),
+                      MyDrawerList(),
+                    ],
+                  ),
+                ),
+              ),
 
-          ),
-        );
-      }
-      Widget MyDrawerList() {
-        return Container(
-          padding: EdgeInsets.only(top: 15,),
-          child: Column(
-            children:[
-              menuItem( 1, "Dashboard", Icons.dashboard_outlined,
-              currentPage == DrawerSections.dashboard ? true : false),
-              menuItem( 2, "Contacts", Icons.people_alt_outlined,
-                  currentPage == DrawerSections.contacts? true : false),
-              menuItem( 3, "Events", Icons.event,
-                  currentPage == DrawerSections.events? true : false),
-              menuItem( 4, "Notes", Icons.notes,
-                  currentPage == DrawerSections.notes? true : false),
-              menuItem( 5, "Settings", Icons.settings_outlined,
-                  currentPage == DrawerSections.settings ? true : false),
-              menuItem( 6, "Notifications", Icons.notifications_outlined,
-                  currentPage == DrawerSections.notifications? true : false),
-              menuItem( 7, "Privacy Policy", Icons.privacy_tip_outlined,
-                  currentPage == DrawerSections.privacy_policy? true : false),
-              menuItem( 8, "Send Feedback", Icons.feed_outlined,
-                  currentPage == DrawerSections.send_feedback ? true : false),
+            ),
+          );
+        }
+        Widget MyDrawerList() {
+          return Container(
+            padding: EdgeInsets.only(top: 15,),
+            child: Column(
+              children: [
+                menuItem(1, "Dashboard", Icons.dashboard_outlined,
+                    currentPage == DrawerSections.dashboard ? true : false),
+                menuItem(2, "Contacts", Icons.people_alt_outlined,
+                    currentPage == DrawerSections.contacts ? true : false),
+                menuItem(3, "Events", Icons.event,
+                    currentPage == DrawerSections.events ? true : false),
+                menuItem(4, "Notes", Icons.notes,
+                    currentPage == DrawerSections.notes ? true : false),
+                menuItem(5, "Settings", Icons.settings_outlined,
+                    currentPage == DrawerSections.settings ? true : false),
+                menuItem(6, "Notifications", Icons.notifications_outlined,
+                    currentPage == DrawerSections.notifications ? true : false),
+                menuItem(7, "Privacy Policy", Icons.privacy_tip_outlined,
+                    currentPage == DrawerSections.privacy_policy
+                        ? true
+                        : false),
+                menuItem(8, "Send Feedback", Icons.feed_outlined,
+                    currentPage == DrawerSections.send_feedback ? true : false),
 
 
-            ],
+              ],
 
-          ),
-        );
-      }
-        Widget menuItem( int id, String title, IconData icon, bool selected){
+            ),
+          );
+        }
+        Widget menuItem(int id, String title, IconData icon, bool selected) {
           return Material(
+            color: selected ? Colors.grey[300] : Colors.transparent,
             child: InkWell(
-              onTap: (){},
+               onTap: () {
+                Navigator.pop(context);
+                setState(() {
+
+                },
+                );
+              },
               child: Padding(
                 padding: EdgeInsets.all(13),
                 child: Row(
@@ -97,16 +134,16 @@ class MyApp extends StatelessWidget {
                     Expanded(
                       flex: 1,
                       child: Icon(
-                     icon,
-                      size: 20,
-                      color: Colors.black,
-                    ),),
+                        icon,
+                        size: 20,
+                        color: Colors.black,
+                      ),),
                     Expanded(
                       child:
-                    Text(title,
-                      style:
-                      TextStyle(color: Colors.black,
-                          fontSize:16 ),), ),
+                      Text(title,
+                        style:
+                        TextStyle(color: Colors.black,
+                            fontSize: 16),),),
 
                   ],
                 ),
@@ -119,6 +156,8 @@ class MyApp extends StatelessWidget {
           );
         }
       }
+
+
       enum DrawerSections{
         dashboard,
         contacts,
